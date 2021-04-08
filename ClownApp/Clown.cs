31d146603f878f1dc1621aaf444c118fd0d514e3
Eduardo Costa Rodrigues <edu.crod@gmail.com>
2021-04-07 22:22:48 -0300

    Add project files.

---
+++ ClownApp/Clown.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClownApp
+{
+    class Clown
+    {
+        public string Name;
+        public int Height;
+        public void TalkAboutYourself()
+        {
+            Console.WriteLine("My name is " + Name + " and I'm " + Height + " inches tall.");
+        }
+    }
+}
